54f1f7c71e802af4ec4336fa3dee18a9e582cf0c
Håvard Trondsen <haavard.trondsen@sparebank1.no>
2023-08-03 15:24:33 +0200

    Litt lengre på fakturaspill, startet såvidt på how to play

---
+++ src/components/FakturaSpill/FakturaIntro.tsx
@@ -0,0 +1,11 @@
+
+
+const FakturaIntro = () => {
+  // TODO How to play the game in here
+  return(
+    <div className={'faktura-spill'}>
+      <div className={'terminal-card'}></div>
+
+    </div>
+  )
+}
--- src/components/FakturaSpill/FakturaSpill.tsx
+++ src/components/FakturaSpill/FakturaSpill.tsx
@@ -1,7 +1,9 @@
 import 'terminal.css'
 import React, {Dispatch, useEffect, useRef, useState} from "react";
+import { Link } from 'react-router-dom';
 
 export const FakturaSpill = () => {
+  const NUM_ROUNDS = 2
   const [ kid, setKid ] = useState<string>()
   const [ konto, setKonto ] = useState<string>()
   const [kontoInput, setKontoInput] = useState<string>("")
@@ -11,11 +13,18 @@ export const FakturaSpill = () => {
 
   const kidInputRef = useRef<HTMLInputElement>(null)
   const kontoInputRef = useRef<HTMLInputElement>(null)
-  
+
+  const [numSuccess, setNumSuccess] = useState<number>(0)
+  const [done, setDone] = useState<boolean>(false)
+  const [startTime, setStartTime] = useState<number>()
+  const [timeUsed, setTimeUsed] = useState<string>("")
+
+  //Initialization
   useEffect(() => {
     generateKonto(setKonto)
     generateKid(setKid)
     if(kidInputRef.current) kidInputRef.current.focus()
+    setStartTime(Date.now())
   }, [])
 
   const reset = () => {
@@ -25,25 +34,33 @@ export const FakturaSpill = () => {
     setKontoSuccess(false)
     if(kidInputRef.current) {
       kidInputRef.current.value = ""
-      //kidInputRef.current.focus() // TODO denne funker ikke fordi kidSuccess er enda true når den blir satt
     }
     if(kontoInputRef.current) {
       kontoInputRef.current.value = ""
     }
+    if(numSuccess === NUM_ROUNDS - 1) {
+      const time = Date.now() - startTime!
+      setTimeUsed(Math.round(time / 1000).toFixed(2))
+      setDone(true)
+    }
+    console.log(`numSuccess: ${numSuccess}`)
+    setNumSuccess((old) => old + 1)
   }
 
+  // Validate input fields and switch focus to konto if kid is success
   useEffect(() => {
+    //KID is success
     if(!kidSuccess && validate(kidInput, kid)) {
       setKidSuccess(true)
       if(kontoInputRef.current) kontoInputRef.current.focus()
-      console.log("KID is success")
     }
+    //Konto is success
     if (!kontoSuccess && validate(kontoInput, konto)) {
       setKontoSuccess(true)
-      console.log("Konto is success")
     }
   }, [kontoInput, kidInput, kid, konto])
 
+  //Check if successfully entered both numbers
   useEffect(() => {
     if(kontoSuccess && kidSuccess) {
       console.log("Resetting: Great success!")
@@ -51,6 +68,7 @@ export const FakturaSpill = () => {
     }
   }, [kontoSuccess, kidSuccess])
 
+  // Change focus to first field on initial and after reset
   useEffect(() => {
     if(!kidSuccess && kidInputRef.current) {
       kidInputRef.current.focus()
@@ -59,11 +77,13 @@ export const FakturaSpill = () => {
 
   return(
     <div className={'faktura-spill'}>
+      {!done &&
       <fieldset>
         <legend>Betale faktura!</legend>
         <div className={"terminal-card"}>
           <header>Betalingsinformasjon</header>
-          <div>Betalingsinfo: Kid: {kid} Kontonr: {konto}</div>
+          <div className={"terminal-alert terminal-alert-primary faktura-spill-info-fields"}>Kid: {kid}</div>
+          <div className={"terminal-alert terminal-alert-primary faktura-spill-info-fields"}>Kontonr: {konto}</div>
         </div>
         <div className={"form-group"}>
           <label htmlFor={"kid"}>KID:</label>
@@ -71,6 +91,7 @@ export const FakturaSpill = () => {
             ref={kidInputRef}
             id={"kid"}
             name={"KID (9 sifferet)"}
+            className={"faktura-spill-input-field"}
             type={"text"}
             disabled={kidSuccess}
             maxLength={9}
@@ -86,13 +107,25 @@ export const FakturaSpill = () => {
             ref={kontoInputRef}
             name={"Konto nummer (11 sifferet)"}
             type={"text"}
+            className={"faktura-spill-input-field"}
             disabled={kontoSuccess}
             maxLength={11}
             placeholder={"Konto nummer (11 sifferet)"}
             onChange={(change) => {setKontoInput(change.currentTarget.value)}}
           />
         </div>
-      </fieldset>
+      </fieldset>}
+      {done &&
+          <div className={'terminal-card'}>
+              <header>Well done!</header>
+              <div>
+                  You completed the challenge in {timeUsed} seconds. Ranking you #x on the scoreboard
+              </div>
+              <Link to={'/'}>
+                <button className={'btn btn-primary'} style={{marginBottom: '15px'}}>Back to menu</button>
+              </Link>
+          </div>
+      }
     </div>
   )
 }
