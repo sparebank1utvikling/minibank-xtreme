94dc5c5820699f42f418fa938f63a22527a1b542
charlotteodegarden <charlotte.odegarden@sparebank1.no>
2024-04-04 15:23:20 +0200

    styling changes fagdag

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -1,41 +1,60 @@
-import './leaderboard.scss'
+import "./leaderboard.less";
 import React, { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { readCSVPromise, ScoreData } from "@/components/Leaderboard/LeaderBoardUtils";
+import {
+  readCSVPromise,
+  ScoreData,
+} from "@/components/Leaderboard/LeaderBoardUtils";
 import { LeaderboardForm } from "@/components/Leaderboard/LeaderboardForm";
 import { LeaderBoardRender } from "@/components/Leaderboard/LeaderBoardRender";
 
 interface Props {
-  gameTitle: string
-  filePath: string
-  registerNew: boolean
-  sortAscending: boolean
-  scoreMetric: string
+  gameTitle: string;
+  filePath: string;
+  registerNew: boolean;
+  sortAscending: boolean;
+  scoreMetric: string;
 }
 
-export const Leaderboard = ({ gameTitle, filePath, registerNew, sortAscending, scoreMetric }: Props) => {
+export const Leaderboard = ({
+  gameTitle,
+  filePath,
+  registerNew,
+  sortAscending,
+  scoreMetric,
+}: Props) => {
   let params = useParams();
-  const newScore = params.score ? parseFloat(params.score) : 0.00
-  const emptyScore: ScoreData[] = []
+  const newScore = params.score ? parseFloat(params.score) : 0.0;
+  const emptyScore: ScoreData[] = [];
 
-  const [scoreBoard, setScoreBoard] = useState(emptyScore)
+  const [scoreBoard, setScoreBoard] = useState(emptyScore);
 
   useEffect(() => {
-    fetchData()
-      .catch(console.error)
-  })
+    fetchData().catch(console.error);
+  });
 
   const fetchData = useCallback(async () => {
     const data = await readCSVPromise(filePath, sortAscending);
-    setScoreBoard(data.splice(0,30));
-  }, [])
+    setScoreBoard(data.splice(0, 30));
+  }, []);
 
   return (
-    <div className="leaderboard" >
-      {registerNew && params.score ?
-        <LeaderboardForm gameTitle={gameTitle} filePath={filePath} scoreBoard={scoreBoard} score={newScore} sortAscending={sortAscending} /> :
-        <LeaderBoardRender gameTitle={gameTitle} scoreBoard={scoreBoard} scoreMetric={scoreMetric} />
-      }
+    <div className="leaderboard">
+      {registerNew && params.score ? (
+        <LeaderboardForm
+          gameTitle={gameTitle}
+          filePath={filePath}
+          scoreBoard={scoreBoard}
+          score={newScore}
+          sortAscending={sortAscending}
+        />
+      ) : (
+        <LeaderBoardRender
+          gameTitle={gameTitle}
+          scoreBoard={scoreBoard}
+          scoreMetric={scoreMetric}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,49 +1,81 @@
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
 import { useRef, useEffect } from "react";
 import { BOARD_PAY_PATH, BOARD_PIN_PATH } from "@/App";
+import SpareBankLogo from "../../assets/rgb_SpareBank1_neg.png";
 
 const Menu = () => {
   const mainMenu = useRef<HTMLDivElement>(null);
   //Initialization
   useEffect(() => {
     if (mainMenu.current != null) {
-      mainMenu.current.focus()
+      mainMenu.current.focus();
     }
-  }, [])
-  const navigate = useNavigate()
+  }, []);
+  const navigate = useNavigate();
   const handleInput = (event: React.KeyboardEvent) => {
     switch (event.key) {
-      case '7':
-        navigate("/pin/intro")
+      case "7":
+        navigate("/pin/intro");
         break;
-      case '9':
-        navigate("/faktura/intro")
+      case "9":
+        navigate("/faktura/intro");
         break;
-      case '1':
+      case "1":
         navigate(BOARD_PIN_PATH);
         break;
-      case '3':
+      case "3":
         navigate(BOARD_PAY_PATH);
         break;
     }
-  }
+  };
   return (
     <>
-      <div className='menu' tabIndex={0} onKeyUp={(event) => handleInput(event)} ref={mainMenu}>
-        <div className={'menu-title'}><h1>MINIBANK XTREME</h1></div>
-        <p className="menu-description">Select by pressing the corresponding number on the numpad</p>
+      <img
+        src={SpareBankLogo}
+        alt="Sparebank 1's logo"
+        className="header-logo"
+      />
+      <div
+        className="menu"
+        tabIndex={0}
+        onKeyUp={(event) => handleInput(event)}
+        ref={mainMenu}
+      >
+        <div className={"menu-title"}>
+          <h1>MINIBANK XTREME</h1>
+        </div>
+        <p className="menu-description">
+          Select by pressing the corresponding number on the numpad
+        </p>
         <div className="menu-grid">
-          <div className={'menu-item--left'}> <h3><Link to={"/pin/intro"}>7) New PIN code</Link></h3></div>
-          <div className={'menu-item--right'}><h3><Link to={"/faktura/intro"}>9) Pay invoice</Link></h3></div>
-          <div className={'menu-item--left'}><h3><Link to={BOARD_PIN_PATH}>1) Top 30 - New PIN code</Link></h3></div>
-          <div className={'menu-item--right'}> <h3><Link to={BOARD_PAY_PATH}>3) Top 30 - Pay invoice</Link></h3></div>
-          
+          <div className={"menu-item--left menu-button"}>
+            {" "}
+            <h3>
+              <Link to={"/pin/intro"}>7) New PIN code</Link>
+            </h3>
+          </div>
+          <div className={"menu-item--right menu-button"}>
+            <h3>
+              <Link to={"/faktura/intro"}>9) Pay invoice</Link>
+            </h3>
+          </div>
+          <div className={"menu-item--left menu-button"}>
+            <h3>
+              <Link to={BOARD_PIN_PATH}>1) Top 30 - New PIN code</Link>
+            </h3>
+          </div>
+          <div className={"menu-item--right menu-button"}>
+            {" "}
+            <h3>
+              <Link to={BOARD_PAY_PATH}>3) Top 30 - Pay invoice</Link>
+            </h3>
+          </div>
         </div>
       </div>
       {/* <p><Link to={`${BOARD_PAY_PATH}/9`}>Leaderboard pay new</Link></p> <p><Link to={`${BOARD_PIN_PATH}/9`}>Leaderboard pin new</Link></p> */}
       {/* kommenter inn det over for å legge til ting i scoreboard uten spille spillene */}
     </>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
