cdae09db04bf0c7d67380565e718a6d52099ec1a
Jens-Christian Bjerkek <jens-christian.bjerkek@sparebank1.no>
2025-02-20 11:46:10 +0100

    --wip-- [skip ci]

--- src/components/games/SpaceInvadersSpill/SpaceInvadersSpill.tsx
+++ src/components/games/SpaceInvadersSpill/SpaceInvadersSpill.tsx
@@ -1,11 +1,32 @@
 import styles from "./SpaceInvadersSpill.module.css";
+import {useState} from "react";
+import {NUMPAD_HOME} from "@/utils/constants";
+import {useNavigate} from "react-router-dom";
 
 export const SpaceInvadersSpill = () => {
+  const [playing, setPlaying] = useState(false);
+  const navigate = useNavigate();
+
+  // Press ESC to return to main menu
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      setPlaying(false);
+      navigate(NUMPAD_HOME);
+    }
+  });
+
+  if (playing) {
+    return (
+      <div className={styles.canvas}>
+        <h1>GAME OVER</h1>
+      </div>
+    )
+
+  }
   return (
     <div className={styles.canvas}>
       <h1>Space Invaders</h1>
-      <p>Spill Space Invaders her</p>
-      <div>løkdjf øalsdkfj aøsdlkfj asdlkfj asdølkdj </div>
+      <button onClick={() => setPlaying(true)}>START</button>
     </div>
   )
 }
