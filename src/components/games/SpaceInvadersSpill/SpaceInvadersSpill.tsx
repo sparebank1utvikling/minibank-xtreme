14ce0ba8a858b7e5049cd7c45cdd53a3d569ce05
Jens-Christian Bjerkek <jens-christian.bjerkek@sparebank1.no>
2025-02-20 11:23:10 +0100

    --wip-- [skip ci]

--- src/components/games/SpaceInvadersSpill/SpaceInvadersSpill.tsx
+++ src/components/games/SpaceInvadersSpill/SpaceInvadersSpill.tsx
@@ -2,10 +2,10 @@ import styles from "./SpaceInvadersSpill.module.css";
 
 export const SpaceInvadersSpill = () => {
   return (
-    <canvas className={styles.canvas}>
+    <div className={styles.canvas}>
       <h1>Space Invaders</h1>
       <p>Spill Space Invaders her</p>
       <div>løkdjf øalsdkfj aøsdlkfj asdlkfj asdølkdj </div>
-    </canvas>
+    </div>
   )
 }
