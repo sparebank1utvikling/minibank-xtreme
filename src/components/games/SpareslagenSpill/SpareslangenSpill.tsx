2b9e0eb7564c1b3272c51e9c27b882e94f84eb94
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2025-02-20 12:03:35 +0100

    Viser kroner spart

--- src/components/games/SpareslagenSpill/SpareslangenSpill.tsx
+++ src/components/games/SpareslagenSpill/SpareslangenSpill.tsx
@@ -5,7 +5,7 @@ import Mynt from './mynt.svg?url';
 const BOARD_SIZE = 20;
 const CELL_SIZE = 25;
 
-type SnakePosition = {
+type Position = {
     x: number;
     y: number;
 };
@@ -21,7 +21,13 @@ const createSnake = () => {
 
 export const SpareslangenSpill = () => {
     const [snakePositions, setSnakePositions] = useState(createSnake());
-    const [coinPosition, setCoinPosition] = useState({ x: 0, y: 0 });
+    const [coinPosition, setCoinPosition] = useState<Position>({ x: 0, y: 0 });
+    const [nokSaved, setNokSaved] = useState(0);
+
+    function handleAteCoin() {
+        setNokSaved(nokSaved + 1);
+        placeNewCoin();
+    }
 
     function placeNewCoin() {
         setCoinPosition({
@@ -30,30 +36,35 @@ export const SpareslangenSpill = () => {
         });
     }
     return (
-    <>
-        <h1>Spareslangen</h1>
-        <div className={styles.board}>
-            <div className={styles.coin} onClick={placeNewCoin}>
-                <img style={{
-                    position: "relative",
-                    top: coinPosition.y * CELL_SIZE,
-                    left: coinPosition.x * CELL_SIZE,
-                    }} src={Mynt} alt="Mynt"
-                />
+        <>
+            <h1>Spareslangen</h1>
+            <span className={styles.savings}>
+                <h2>Så mye har du spart <span style={{color: "orange"}}>{nokSaved}</span></h2>
+                <img style={{width: CELL_SIZE, height: CELL_SIZE}} src={Mynt} alt="Mynt"/>
+            </span>
+
+            <div className={styles.board}>
+                <div className={styles.coin} onClick={handleAteCoin}>
+                    <img style={{
+                        position: "relative",
+                        top: coinPosition.y * CELL_SIZE,
+                        left: coinPosition.x * CELL_SIZE,
+                        }} src={Mynt} alt="Mynt"
+                    />
+                </div>
             </div>
-        </div>
-        {snakePositions.map((snakePosition: SnakePosition) => (
-            <div
-                style={{
-                    position: "absolute",
-                    top: `${snakePosition.y * 10}px`,
-                    left: `${snakePosition.x * 10}px`,
-                    width: "10px",
-                    height: "10px",
-                    backgroundColor: "green",
-                }}
-            />
-      ))}
-    </>
+            {snakePositions.map((snakePosition: Position) => (
+                <div
+                    style={{
+                        position: "absolute",
+                        top: `${snakePosition.y * 10}px`,
+                        left: `${snakePosition.x * 10}px`,
+                        width: "10px",
+                        height: "10px",
+                        backgroundColor: "green",
+                    }}
+                />
+          ))}
+        </>
     );
 };
